e47e4f9eaf90e8d66495a5d0b012b8b90550da76
Gopichandu923 <gopichandu6080@gmail.com>
2025-05-26 22:27:46 +0530

    modified the logic

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,38 @@
 import mongoose from "mongoose";
 import colors from "colors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`MongoDB connected ;${conn.connection.host}`.cyan.underline);
+    const conn = await mongoose.connect(process.env.MONGO_URL, {
+      serverSelectionTimeoutMS: 5000, // Timeout after 5s
+      maxPoolSize: 10, // Maintain up to 10 socket connections
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
+
+    // Connection event listeners
+    mongoose.connection.on("connected", () => {
+      console.log("Mongoose connected to DB".blue);
+    });
+
+    mongoose.connection.on("error", (err) => {
+      console.error(`Mongoose connection error: ${err.message}`.red);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("Mongoose disconnected".yellow);
+    });
+
+    // Close connection on process termination
+    process.on("SIGINT", async () => {
+      await mongoose.connection.close();
+      console.log("Mongoose connection closed due to app termination".red);
+      process.exit(0);
+    });
   } catch (err) {
-    console.error(`Error:${err.message}`.red.underline.bold);
+    console.error(`Error: ${err.message}`.red.underline.bold);
     process.exit(1);
   }
 };
