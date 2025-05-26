94090fed730fe1a108c39d9d3cdfba55cd5ee1d3
Gopichandu923 <gopichandu6080@gmail.com>
2025-05-26 21:32:44 +0530

    added the auth routes to  server

---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/database.js";
+import colors from "colors";
+
+import authRoutes from "./routes/authRoutes.js";
+
+dotenv.config();
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("api/user", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API is running.......");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`.yellow.bold);
+});
