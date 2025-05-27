c10bdd78684b4e965e6aec2b038d12bbc48955a2
Gopichandu923 <gopichandu6080@gmail.com>
2025-05-28 00:27:37 +0530

    updted the App component for routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Cart from "./pages/Cart.js";
+import Product from "./pages/Product.js";
+import Profile from "./pages/Profile.js";
+import Login from "./pages/Login.js";
+import Register from "./pages/Register";
+import Order from "./pages/Order.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/products" element={<Product />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/orders" element={<Order />} />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
