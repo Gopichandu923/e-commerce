fddca4f2419862951b78ab78233a549c342a182d
Gopichandu923 <gopichandu6080@gmail.com>
2025-05-30 23:12:43 +0530

    added the favourites controller

---
+++ server/controllers/favouriteController.js
@@ -0,0 +1,60 @@
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+const addToFavorites = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const productId = req.params.productId;
+
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    res.status(400);
+    throw new Error("Invalid product ID");
+  }
+
+  const user = await User.findById(userId);
+
+  if (user.favorites.includes(productId)) {
+    res.status(400);
+    throw new Error("Product already in favorites");
+  }
+
+  user.favorites.push(productId);
+  const updatedUser = await user.save();
+
+  res.status(200).json({
+    message: "Product added to favorites",
+    favorites: updatedUser.favorites,
+  });
+});
+
+const removeFromFavorites = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  const productId = req.params.productId;
+
+  if (!mongoose.Types.ObjectId.isValid(productId)) {
+    res.status(400);
+    throw new Error("Invalid product ID");
+  }
+
+  const user = await User.findById(userId);
+
+  if (!user.favorites.includes(productId)) {
+    res.status(404);
+    throw new Error("Product not found in favorites");
+  }
+
+  user.favorites = user.favorites.filter((id) => id.toString() !== productId);
+  const updatedUser = await user.save();
+
+  res.status(200).json({
+    message: "Product removed from favorites",
+    favorites: updatedUser.favorites,
+  });
+});
+
+const getFavorites = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+
+  const user = await User.findById(userId).populate("favorites");
+
+  res.status(200).json(user.favorites);
+});
