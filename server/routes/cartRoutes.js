8b0edb41c13494c5ed6fa873e2e05947fda89659
Gopichandu923 <gopichandu6080@gmail.com>
2025-05-30 23:56:12 +0530

    added cart routes

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+  addToCart,
+  updateCartItem,
+  removeFromCart,
+  getCart,
+  clearCart,
+} from "../controllers/cartController.js";
+
+const router = express.Router();
+
+router.route("/").get(protect, getCart).delete(protect, clearCart);
+router.route("/add").post(protect, addToCart);
+router
+  .route("/:itemId")
+  .put(protect, updateCartItem)
+  .delete(protect, removeFromCart);
