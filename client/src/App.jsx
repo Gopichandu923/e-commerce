ceb788a414f427378a257cdc232a368ddf0cd0c2
Gopichandu923 <gopichandu6080@gmail.com>
2025-06-05 01:29:14 +0530

    updated the routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/HomePage.jsx";
 import Cart from "./pages/CartPage.jsx";
 import Favourite from "./pages/FavouritePage.jsx";
-import Product from "./pages/ProductPage.jsx";
+import ProductDetails from "./pages/ProductDetails.jsx";
 import Profile from "./pages/ProfilePage.jsx";
 import Login from "./pages/LoginPage.jsx";
 import Register from "./pages/RegisterPage.jsx";
 import Order from "./pages/OrderPage.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import ShopPage from "./pages/shoppingPage.jsx";
 
 const App = () => {
   return (
@@ -20,12 +21,14 @@ const App = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/products" element={<Product />} />
+            <Route path="/product/:productId" element={<ProductDetails />} />
             <Route path="/profile" element={<Profile />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/orders" element={<Order />} />
             <Route path="/favourite" element={<Favourite />} />
+            <Route path="/shop" element={<ShopPage />} />
+            <Route path="/shop/:categoryName" element={<ShopPage />} />
           </Routes>
         </main>
         <Footer />
