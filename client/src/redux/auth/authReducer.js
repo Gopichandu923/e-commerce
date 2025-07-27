c76be149e1d023caa95c225d851459f376b5702f
Gopichandu923 <gopichandu6080@gmail.com>
2025-07-27 10:53:12 +0530

    added extra states and actions

--- client/src/redux/auth/authReducer.js
+++ client/src/redux/auth/authReducer.js
@@ -3,21 +3,41 @@ const user = localStorage.getItem("user")
   : null;
 
 const initialState = {
-  user: user ? user : null,
-  error: null,
+  user: user || null,
   loading: false,
+  error: null,
+  successMessage: null,
 };
 
 function authReducer(state = initialState, action) {
   switch (action.type) {
-    case "LOGIN":
-    case "REGISTER":
-      return { ...state, user: action.payload, error: null };
-    case "LOGIN_ERROR":
-    case "REGISTER_ERROR":
-      return { ...state, error: action.payload };
+    case "REGISTER_REQUEST":
+    case "LOGIN_REQUEST":
+      return { ...state, loading: true, error: null, successMessage: null };
+
+    case "REGISTER_SUCCESS":
+    case "LOGIN_SUCCESS":
+      return {
+        ...state,
+        user: action.payload,
+        loading: false,
+        error: null,
+        successMessage: action.message || null,
+      };
+
+    case "REGISTER_FAILURE":
+    case "LOGIN_FAILURE":
+      return { ...state, loading: false, error: action.payload };
+
     case "LOGOUT":
-      return { ...state, user: null };
+      return {
+        ...state,
+        user: null,
+        loading: false,
+        error: null,
+        successMessage: null,
+      };
+
     default:
       return state;
   }
