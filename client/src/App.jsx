61d845b9b8a88d79991a0affd35ec01452c14bd2
Gopichandu923 <gopichandu6080@gmail.com>
2026-04-08 08:07:25 +0530

    updated the cart count in navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Home from "./pages/HomePage.jsx";
 import Cart from "./pages/CartPage.jsx";
 import Favourite from "./pages/FavouritePage.jsx";
@@ -11,12 +12,33 @@ import Order from "./pages/OrderPage.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import ShopPage from "./pages/shoppingPage.jsx";
+import SearchPage from "./pages/SearchPage.jsx";
+import CheckoutPage from "./pages/CheckoutPage.jsx";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    if (typeof window !== "undefined") {
+      return localStorage.getItem("darkMode") === "true";
+    }
+    return false;
+  });
+
+  const { cartItems } = useSelector((state) => state.cart);
+  const cartCount = useMemo(() => cartItems.reduce((sum, item) => sum + item.quantity, 0), [cartItems]);
+
+  useEffect(() => {
+    localStorage.setItem("darkMode", darkMode);
+    if (darkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [darkMode]);
+
   return (
     <Router>
-      <div className="app-container">
-        <Header />
+      <div className={`app-container ${darkMode ? "dark" : ""}`}>
+        <Header darkMode={darkMode} setDarkMode={setDarkMode} cartCount={cartCount} />
         <main className="main-content">
           <Routes>
             <Route path="/" element={<Home />} />
@@ -29,6 +51,8 @@ const App = () => {
             <Route path="/favourite" element={<Favourite />} />
             <Route path="/shop" element={<ShopPage />} />
             <Route path="/shop/:categoryName" element={<ShopPage />} />
+            <Route path="/search" element={<SearchPage darkMode={darkMode} />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
           </Routes>
         </main>
         <Footer />
