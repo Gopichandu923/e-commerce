2347c17a7a73c18d3aaae9b5fcd71337e6fa82c2
Gopichandu923 <gopichandu6080@gmail.com>
2026-04-08 08:15:38 +0530

    included the cart slice in store

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./auth/authReducer.js";
 import favouriteReducer from "./favourites/favouriteReducer.js";
+import cartReducer from "./cart/cartSlice.js";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     favourites: favouriteReducer,
+    cart: cartReducer,
   },
+  middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export default store;
